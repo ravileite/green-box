77373f295b551390fd38c217c4e7d981398d1862
Wesley Silva <wesleyxoliveiraxsilva@gmail.com>
2016-07-23 01:58:42 -0300

    First Commit

---
+++ green-box/src/main/java/org/ufcg/si/GreenBoxApplication.java
@@ -0,0 +1,12 @@
+package org.ufcg.si;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenBoxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreenBoxApplication.class, args);
+	}
+}
