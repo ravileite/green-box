16a6758307649f7216a3b7e52cb91675d7489445
Natan7 <natan.ribeiro@ccc.ufcg.edu.br>
2016-08-05 18:45:28 -0300

    add save txt in file.html

--- green-box/src/main/resources/static/js/controllers/file-controller.js
+++ green-box/src/main/resources/static/js/controllers/file-controller.js
@@ -1,10 +1,18 @@
 $('.summernote').summernote({
 	height: 300,   //set editable area's height
 	codemirror: { // codemirror options
-	theme: 'monokai'
+		theme: 'monokai'
 	}
-	});
+});
 
 $(document).ready(function() {
-$('#summernote').summernote('justifyLeft');});
-  
+	$('#summernote').summernote('justifyLeft');
+});
+
+// save texts of html page 
+ var save = function() {
+ 	  var makrup = $('.click2edit').summernote('code');
+ 	  $('.click2edit').summernote('destroy');
+ };
+
+
