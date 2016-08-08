bfb1a82a06e1493faceb57f87518f5b07582fa9a
Wesley Silva <wesleyxoliveiraxsilva@gmail.com>
2016-08-08 14:07:59 -0300

    Removed useless files and inserted an annotation in UserRepository.

--- green-box/src/main/java/org/ufcg/si/repositories/UserRepository.java
+++ green-box/src/main/java/org/ufcg/si/repositories/UserRepository.java
@@ -3,9 +3,10 @@ package org.ufcg.si.repositories;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 import org.ufcg.si.models.User;
 
-
+@Repository
 public interface UserRepository extends CrudRepository<User, Long>{
 	
 	@Query(value = "Select user from User user where user.username=:username")
